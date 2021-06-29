27f411d81860ebf55b0348d2d01425086a3b799b
David Peña <jpenha1219@gmail.com>
2021-06-29 18:05:00 -0500

    Pregunta 1

--- app/src/main/java/com/example/aceleracion/MainActivity.java
+++ app/src/main/java/com/example/aceleracion/MainActivity.java
@@ -2,13 +2,84 @@ package com.example.aceleracion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private TextView ejex;
+    private TextView ejey;
+    private TextView ejez;
+    private String a, b, c;
+    private float[] gravity = new float[3];
+    final float ALPHA =  0.8f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ejex = findViewById(R.id.tv_eje_x);
+        ejey = findViewById(R.id.tv_eje_y);
+        ejez = findViewById(R.id.tv_eje_z);
+        mSensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this,mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSensorManager = null;
+        mAccelerometer = null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values.clone();
+
+        values = highPass(values[0], values[1], values[2]);
+        a = values[0]+"";
+        b = values[1]+"";
+        c = values[1]+"";
+        ejex.setText(a);
+        ejey.setText(b);
+        ejez.setText(c);
+
+        //aca ejecutan lo de la suma de cuadrados para captar el desplazamiento
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+//METODO PARA HACER EL FILTRO
+    private float[] highPass(float x, float y, float z)
+    {
+        float[] filteredValues = new float[3];
+        gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * x;
+        gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * y;
+        gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * z;
+        filteredValues[0] = x - gravity[0];
+        filteredValues[1] = y - gravity[1];
+        filteredValues[2] = z - gravity[2];
+        return filteredValues;
+    }
+//metodo multiplicacion de cuadrados
+    //aca deben de poner y meterlo dentro de onchanged
 }
